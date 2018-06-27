a3dea9664abfcd68d014e4fccf359be18a0bc9ab
pxgamer <owzie123@gmail.com>
2018-06-27 11:57:43 +0100

    Remove PHPCS checking for line length on key

--- tests/ArionumTest.php
+++ tests/ArionumTest.php
@@ -11,7 +11,9 @@ class ArionumTest extends TestCase
 {
     const TEST_NODE = 'https://aro.pxgamer.xyz';
     const TEST_ADDRESS = '51sJ4LbdKzhyGy4zJGqodNLse9n9JsVT2rdeH92w7cf3qQuSDJupvjbUT1UBr7r1SCUAXG97saxn7jt2edKb4v4J';
+    // phpcs:disable Generic.Files.LineLength
     const TEST_PUBLIC_KEY = 'PZ8Tyr4Nx8MHsRAGMpZmZ6TWY63dXWSCyk7aKeBJ6LL44w5JGSFp82Wb1Drqicuznv1qmRVQMvbmF64AeczjMtV72acGLR9RsiQ2JccemNrSPkKi8KDk72t4';
+    // phpcs:enable
 
     /**
      * @var Arionum
