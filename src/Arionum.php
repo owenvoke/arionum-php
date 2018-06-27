195cc91f476b0f26b6a1bea003e20f258a289a69
pxgamer <owzie123@gmail.com>
2018-06-27 11:11:44 +0100

    Add initial Arionum class

---
+++ src/Arionum.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace pxgamer\Arionum;
+
+/**
+ * Class Arionum
+ */
+class Arionum
+{
+    /**
+     * The request endpoint for API calls.
+     */
+    const API_ENDPOINT = '/api.php';
+}
