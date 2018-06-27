1b3d6e4298f91fcef4a8c557871aa8b72245633e
pxgamer <owzie123@gmail.com>
2018-06-27 11:21:09 +0100

    Add nodeAddress and client methods

--- src/Arionum.php
+++ src/Arionum.php
@@ -2,6 +2,8 @@
 
 namespace pxgamer\Arionum;
 
+use GuzzleHttp\Client;
+
 /**
  * Class Arionum
  */
@@ -11,4 +13,40 @@ class Arionum
      * The request endpoint for API calls.
      */
     const API_ENDPOINT = '/api.php';
+
+    /**
+     * @var string
+     */
+    private $nodeAddress;
+    /**
+     * @var Client
+     */
+    private $client;
+
+    /**
+     * @return string
+     */
+    public function getNodeAddress(): string
+    {
+        return $this->nodeAddress;
+    }
+
+    /**
+     * @param string $nodeAddress
+     */
+    public function setNodeAddress(string $nodeAddress): void
+    {
+        $this->nodeAddress = $nodeAddress;
+    }
+
+    public function getClient()
+    {
+        if (!$this->client instanceof Client) {
+            $this->client = new Client([
+                'base_uri' => $this->nodeAddress,
+            ]);
+        }
+
+        return $this->client;
+    }
 }
