ceb7873c1525452b09b996389557b2f6458b2b6a
pxgamer <owzie123@gmail.com>
2018-06-28 09:27:10 +0100

    Add Transaction class

---
+++ src/Transaction.php
@@ -0,0 +1,181 @@
+<?php
+
+namespace pxgamer\Arionum;
+
+/**
+ * Class Transaction
+ */
+class Transaction
+{
+    /**
+     * The value to send in the transaction.
+     * @var float
+     */
+    private $val;
+    /**
+     * The destination address.
+     * @var string
+     */
+    private $dst;
+    /**
+     * The sender's public key.
+     * @var string
+     */
+    private $public_key;
+    /**
+     * The transaction signature.
+     * It's recommended that the transaction is signed to avoid sending your private key to the node.
+     * @var string
+     */
+    private $signature;
+    /**
+     * The sender's private key. Only required if no signature is provided.
+     * @var string
+     */
+    private $private_key;
+    /**
+     * The transaction date in unix timestamp format.
+     * This is required when the transaction is pre-signed.
+     * @var int
+     * @see https://epochconverter.com
+     */
+    private $date;
+    /**
+     * A message to be included with the transaction. Maximum 128 chars.
+     * @var string
+     */
+    private $message;
+    /**
+     * The version of the transaction.
+     * @var int
+     */
+    private $version = 1;
+
+    /**
+     * @return float
+     */
+    public function getValue(): float
+    {
+        return $this->val;
+    }
+
+    /**
+     * @param float $val
+     */
+    public function setValue(float $val): void
+    {
+        $this->val = $val;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDestinationAddress(): string
+    {
+        return $this->dst;
+    }
+
+    /**
+     * @param string $dst
+     */
+    public function setDestinationAddress(string $dst): void
+    {
+        $this->dst = $dst;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPublicKey(): string
+    {
+        return $this->public_key;
+    }
+
+    /**
+     * @param string $public_key
+     */
+    public function setPublicKey(string $public_key): void
+    {
+        $this->public_key = $public_key;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSignature(): string
+    {
+        return $this->signature;
+    }
+
+    /**
+     * @param string $signature
+     */
+    public function setSignature(string $signature): void
+    {
+        $this->signature = $signature;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPrivateKey(): string
+    {
+        return $this->private_key;
+    }
+
+    /**
+     * @param string $private_key
+     */
+    public function setPrivateKey(string $private_key): void
+    {
+        $this->private_key = $private_key;
+    }
+
+    /**
+     * @return int
+     */
+    public function getDate(): int
+    {
+        return $this->date;
+    }
+
+    /**
+     * @param int $date
+     */
+    public function setDate(int $date): void
+    {
+        $this->date = $date;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMessage(): string
+    {
+        return $this->message;
+    }
+
+    /**
+     * @param string $message
+     */
+    public function setMessage(string $message): void
+    {
+        $this->message = $message;
+    }
+
+    /**
+     * @return int
+     */
+    public function getVersion(): int
+    {
+        return $this->version;
+    }
+
+    /**
+     * @param int $version
+     */
+    public function setVersion(int $version): void
+    {
+        $this->version = $version;
+    }
+}
