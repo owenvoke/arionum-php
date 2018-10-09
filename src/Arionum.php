7ae3927fa607adf4eba6d6bee9cb6a470beaf044
pxgamer <owzie123@gmail.com>
2018-10-09 09:54:39 +0100

    Add deprecation warning for setNodeAddress

--- src/Arionum.php
+++ src/Arionum.php
@@ -298,6 +298,7 @@ class Arionum
     }
 
     /**
+     * @deprecated 2.0.0 This will no longer be available and should be set through the constructor.
      * @param string $nodeAddress
      */
     public function setNodeAddress(string $nodeAddress): void
