2829d2bce2395194d78698710c6df46615ae5e94
pxgamer <owzie123@gmail.com>
2019-02-11 16:26:33 +0000

    Add new Exception interface

---
+++ src/Exception/ExceptionInterface.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace pxgamer\Arionum\Exception;
+
+interface ExceptionInterface
+{
+}
---
+++ src/Exception/HttpException.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace pxgamer\Arionum\Exception;
+
+class HttpException extends \RuntimeException implements ExceptionInterface
+{
+}
