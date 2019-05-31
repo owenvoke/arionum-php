9f225b4cebda808d6e19a5af849dc14aff9d60e9
pxgamer <owzie123@gmail.com>
2019-05-31 15:53:12 +0100

    Add Laravel configuration file

---
+++ src/Laravel/config/arionum.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Remote Node URI
+    |--------------------------------------------------------------------------
+    |
+    | This option controls the node that will be used to call the API methods
+    | of the package. This will be bound as a singleton to the Laravel app.
+    |
+    */
+
+    'node-uri' => env('ARIONUM_NODE_URI'),
+
+];
