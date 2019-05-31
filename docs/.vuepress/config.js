a0f6ee370cf9d04300482648476334edee43ee62
pxgamer <owzie123@gmail.com>
2019-05-31 15:55:09 +0100

    Add initial VuePress configuration

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  title: 'Arionum PHP',
+  description: 'An API wrapper for the Arionum cryptocurrency node.',
+  serviceWorker: true,
+  sidebar: true,
+  themeConfig: {
+    repo: 'pxgamer/arionum-php',
+    docsDir: 'docs',
+    editLinks: true
+  }
+}
