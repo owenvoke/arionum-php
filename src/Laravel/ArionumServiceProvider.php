4afe65ef80017de074444592e3ac71d9a60d274b
pxgamer <owzie123@gmail.com>
2019-06-03 11:43:56 +0100

    Merge configuration if not published

--- src/Laravel/ArionumServiceProvider.php
+++ src/Laravel/ArionumServiceProvider.php
@@ -3,8 +3,8 @@
 namespace pxgamer\Arionum\Laravel;
 
 use pxgamer\Arionum\Arionum;
-use pxgamer\Arionum\Exceptions\InvalidNodeUriException;
 use Illuminate\Support\ServiceProvider;
+use pxgamer\Arionum\Exceptions\InvalidNodeUriException;
 
 final class ArionumServiceProvider extends ServiceProvider
 {
@@ -13,12 +13,14 @@ final class ArionumServiceProvider extends ServiceProvider
         $this->publishes([
             __DIR__.'/config/arionum.php' => $this->app->configPath('arionum.php'),
         ], 'config');
+
+        $this->mergeConfigFrom(__DIR__.'/config/arionum.php', 'arionum');
     }
 
     public function register(): void
     {
         $this->app->singleton(Arionum::class, function () {
-            if (!$this->app->get('config')->get('arionum.node-uri')) {
+            if (! $this->app->get('config')->get('arionum.node-uri')) {
                 throw new InvalidNodeUriException('The configured node uri is invalid. A valid `ARIONUM_NODE_URI` variable should be configured in your environment');
             }
 
