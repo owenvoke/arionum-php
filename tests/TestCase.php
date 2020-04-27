8743bac7bb4e29e978622afed88fe79032ebd568
Owen Voke <development@voke.dev>
2020-04-27 18:32:57 +0100

    Fix Style CI issues

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace OwenVoke\Arionum\Tests;
 
-use PHPUnit\Framework\TestCase as BaseTestCase;
 use OwenVoke\Arionum\Arionum;
+use PHPUnit\Framework\TestCase as BaseTestCase;
 
 class TestCase extends BaseTestCase
 {
