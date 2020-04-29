3a67a0f5b5e1707df37abc29619cb2033c985f90
Owen Voke <development@voke.dev>
2020-04-29 20:15:20 +0100

    tests: update NodeTest class to Pest

--- tests/Feature/NodeTest.php
+++ tests/Feature/NodeTest.php
@@ -2,8 +2,6 @@
 
 declare(strict_types=1);
 
-namespace OwenVoke\Arionum\Tests\Feature;
-
 use GuzzleHttp\Client;
 use GuzzleHttp\Handler\MockHandler;
 use GuzzleHttp\HandlerStack;
@@ -12,82 +10,58 @@ use OwenVoke\Arionum\Arionum;
 use OwenVoke\Arionum\Exceptions\GenericApiException;
 use OwenVoke\Arionum\Tests\TestCase;
 
-final class NodeTest extends TestCase
-{
-    /**
-     * @test
-     * @return void
-     */
-    public function itGetsTheNodeAddress(): void
-    {
-        $data = $this->arionum->getNodeAddress();
-        $this->assertEquals(self::TEST_NODE, $data);
-    }
+beforeEach()->withArionum();
 
-    /**
-     * @test
-     * @return void
-     * @throws GenericApiException
-     */
-    public function itGetsTheVersionForTheCurrentNode(): void
-    {
-        $data = $this->arionum->getNodeVersion();
-        $this->assertNotEmpty($data);
-    }
+it('getsTheNodeAddress', function (): void {
+    $data = $this->arionum->getNodeAddress();
+    assertEquals(TestCase::TEST_NODE, $data);
+});
 
-    /**
-     * @test
-     * @return void
-     * @throws GenericApiException
-     */
-    public function itGetsTheSanityDetailsForTheCurrentNode(): void
-    {
-        $data = $this->arionum->getSanityDetails();
+it('gets the version for the current node', function (): void {
+    $data = $this->arionum->getNodeVersion();
+    assertNotEmpty($data);
+});
 
-        $this->assertIsBool($data->sanity_running);
-        $this->assertIsNumeric($data->last_sanity);
-        $this->assertIsBool($data->sanity_sync);
-    }
+it('gets the sanity details for the current node', function (): void {
+    $data = $this->arionum->getSanityDetails();
 
-    /**
-     * @test
-     * @return void
-     * @throws GenericApiException
-     */
-    public function itGetsTheNodeInfoForTheCurrentNode(): void
-    {
-        $mock = new MockHandler([
-            new Response(200, [], json_encode([
-                'status' => 'ok',
-                'data' => [
-                    'hostname' => 'https://aro.example.com',
-                    'version' => '0.4.5',
-                    'dbversion' => '9',
-                    'accounts' => 14817,
-                    'transactions' => 2779519,
-                    'mempool' => 8,
-                    'masternodes' => 484,
-                    'peers' => 108,
-                ],
-                'coin' => 'arionum',
-            ])),
-        ]);
+    assertIsBool($data->sanity_running);
+    assertIsNumeric($data->last_sanity);
+    assertIsBool($data->sanity_sync);
+});
 
-        $handler = HandlerStack::create($mock);
-        $client = new Client(['handler' => $handler]);
+it('gets the information for the current node', function (): void {
+    $mock = new MockHandler([
+        new Response(200, [], json_encode([
+            'status' => 'ok',
+            'data' => [
+                'hostname' => 'https://aro.example.com',
+                'version' => '0.4.5',
+                'dbversion' => '9',
+                'accounts' => 14817,
+                'transactions' => 2779519,
+                'mempool' => 8,
+                'masternodes' => 484,
+                'peers' => 108,
+            ],
+            'coin' => 'arionum',
+        ], JSON_THROW_ON_ERROR)),
+    ]);
 
-        $this->arionum = new Arionum(self::TEST_NODE, $client);
+    $handler = HandlerStack::create($mock);
+    $client = new Client(['handler' => $handler]);
 
-        $data = $this->arionum->getNodeInfo();
+    $this->withArionum(null, $client);
 
-        $this->assertIsString($data->hostname);
-        $this->assertIsString($data->version);
-        $this->assertIsString($data->dbversion);
-        $this->assertIsInt($data->accounts);
-        $this->assertIsInt($data->transactions);
-        $this->assertIsInt($data->mempool);
-        $this->assertIsInt($data->masternodes);
+    $data = $this->arionum->getNodeInfo();
 
-        $this->assertEquals('https://aro.example.com', $data->hostname);
-    }
-}
+    assertIsString($data->hostname);
+    assertIsString($data->version);
+    assertIsString($data->dbversion);
+    assertIsInt($data->accounts);
+    assertIsInt($data->transactions);
+    assertIsInt($data->mempool);
+    assertIsInt($data->masternodes);
+
+    assertEquals('https://aro.example.com', $data->hostname);
+});
