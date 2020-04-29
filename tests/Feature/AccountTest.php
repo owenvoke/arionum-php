22c26c91972e6c2fd75dd9ba3ae322d73e8cded6
Owen Voke <development@voke.dev>
2020-04-29 19:42:43 +0100

    tests: update AccountTest class to Pest

--- tests/Feature/AccountTest.php
+++ tests/Feature/AccountTest.php
@@ -2,112 +2,55 @@
 
 declare(strict_types=1);
 
-namespace OwenVoke\Arionum\Tests\Feature;
+beforeEach()->withArionum();
 
-use OwenVoke\Arionum\Exceptions\GenericApiException;
-use OwenVoke\Arionum\Tests\TestCase;
+$testAddress = '51sJ4LbdKzhyGy4zJGqodNLse9n9JsVT2rdeH92w7cf3qQuSDJupvjbUT1UBr7r1SCUAXG97saxn7jt2edKb4v4J';
+$testPublicKey = 'PZ8Tyr4Nx8MHsRAGMpZmZ6TWY63dXWSCyk7aKeBJ6LL44w5JGSFp82Wb1Drqicuznv1qmRVQMvbmF64AeczjMtV72acGLR9RsiQ2JccemNrSPkKi8KDk72t4';
 
-final class AccountTest extends TestCase
-{
-    /**
-     * @test
-     * @return void
-     * @throws GenericApiException
-     */
-    public function itGeneratesANewAccount(): void
-    {
-        $data = $this->arionum->generateAccount();
-        $this->assertStringStartsWith('PZ', $data->getPublicKey());
-        $this->assertStringStartsWith('Lz', $data->getPrivateKey());
-    }
+it('generates a new account', function (): void {
+    $data = $this->arionum->generateAccount();
+    assertStringStartsWith('PZ', $data->getPublicKey());
+    assertStringStartsWith('Lz', $data->getPrivateKey());
+});
 
-    /**
-     * @test
-     * @return void
-     * @throws GenericApiException
-     */
-    public function itGetsTheBalanceForATestAddress(): void
-    {
-        $data = $this->arionum->getBalance(self::TEST_ADDRESS);
-        $this->assertIsNumeric($data);
-    }
+it('gets the balance for a test address', function () use ($testAddress): void {
+    $data = $this->arionum->getBalance($testAddress);
+    assertIsNumeric($data);
+});
 
-    /**
-     * @test
-     * @return void
-     * @throws GenericApiException
-     */
-    public function itGetsTheBalanceForATestAlias(): void
-    {
-        $data = $this->arionum->getBalanceByAlias('PXGAMER');
-        $this->assertIsNumeric($data);
-    }
+it('gets the balance for a test alias', function (): void {
+    $data = $this->arionum->getBalanceByAlias('PXGAMER');
+    assertIsNumeric($data);
+});
 
-    /**
-     * @test
-     * @return void
-     * @throws GenericApiException
-     */
-    public function itGetsTheBalanceForATestPublicKey(): void
-    {
-        $data = $this->arionum->getBalanceByPublicKey(self::TEST_PUBLIC_KEY);
-        $this->assertIsNumeric($data);
-    }
+it('gets the balance for a public key', function () use ($testPublicKey): void {
+    $data = $this->arionum->getBalanceByPublicKey($testPublicKey);
+    assertIsNumeric($data);
+});
 
-    /**
-     * @test
-     * @return void
-     * @throws GenericApiException
-     */
-    public function itGetsThePendingBalanceForATestAddress(): void
-    {
-        $data = $this->arionum->getPendingBalance(self::TEST_ADDRESS);
-        $this->assertIsNumeric($data);
-    }
+it('gets the pending balance for a public key', function () use ($testPublicKey): void {
+    $data = $this->arionum->getPendingBalance($testPublicKey);
+    assertIsNumeric($data);
+});
 
-    /**
-     * @test
-     * @return void
-     * @throws GenericApiException
-     */
-    public function itGetsTheTransactionsForATestAddress(): void
-    {
-        $data = $this->arionum->getTransactions(self::TEST_ADDRESS);
-        $this->assertIsArray($data);
-        $this->assertNotEmpty($data);
-    }
+it('gets the transactions for an address', function () use ($testAddress): void {
+    $data = $this->arionum->getTransactions($testAddress);
+    assertIsArray($data);
+    assertNotEmpty($data);
+});
 
-    /**
-     * @test
-     * @return void
-     * @throws GenericApiException
-     */
-    public function itGetsTheTransactionsForATestPublicKey(): void
-    {
-        $data = $this->arionum->getTransactionsByPublicKey(self::TEST_PUBLIC_KEY);
-        $this->assertIsArray($data);
-        $this->assertNotEmpty($data);
-    }
+it('gets the transactions for a public key', function () use ($testPublicKey): void {
+    $data = $this->arionum->getTransactionsByPublicKey($testPublicKey);
+    assertIsArray($data);
+    assertNotEmpty($data);
+});
 
-    /**
-     * @test
-     * @return void
-     * @throws GenericApiException
-     */
-    public function itGetsTheAliasForASpecificAddress(): void
-    {
-        $data = $this->arionum->getAlias(self::TEST_ADDRESS);
-        $this->assertEquals('PXGAMER', $data);
-    }
+it('gets the alias for an address', function () use ($testAddress): void {
+    $data = $this->arionum->getAlias($testAddress);
+    assertEquals('PXGAMER', $data);
+});
 
-    /**
-     * @test
-     * @return void
-     * @throws GenericApiException
-     */
-    public function itChecksThatTheAddressIsValid(): void
-    {
-        $data = $this->arionum->checkAddress(self::TEST_ADDRESS);
-        $this->assertTrue($data);
-    }
-}
+it('checks that an address is valid', function () use ($testAddress): void {
+    $data = $this->arionum->checkAddress($testAddress);
+    assertTrue($data);
+});
