e62cb57b47afa764eceb33483ffd57247e8c48ab
Owen Voke <development@voke.dev>
2021-05-06 10:13:03 +0100

    tests: update OtherTest class to Pest

--- tests/Feature/OtherTest.php
+++ tests/Feature/OtherTest.php
@@ -2,66 +2,43 @@
 
 declare(strict_types=1);
 
-namespace OwenVoke\Arionum\Tests\Feature;
-
+use OwenVoke\Arionum\Arionum;
 use OwenVoke\Arionum\Exceptions\GenericApiException;
 use OwenVoke\Arionum\Models\Transaction;
 use OwenVoke\Arionum\Tests\TestCase;
 use OwenVoke\Arionum\Transaction\Version;
 
-final class OtherTest extends TestCase
-{
-    // phpcs:disable Generic.Files.LineLength
-    private const TEST_SEND_KEY1 = 'PZ8Tyr4Nx8MHsRAGMpZmZ6TWY63dXWSD1Hm7fGpQAgh1goGj8G47RmU68i3mP4erGGrJ1LNBzEy4di4jZKA2Z6ee96VxaDMUnzSthyzMSyhqF1DbLwNKPim2';
-    private const TEST_SEND_KEY2 = 'Lzhp9LopCDbzk3eSdzuL5f9cR9ng12s6gNonQET3kSLtZU4MbQVreDRFjoWcEdUyeUN3tKwpR4AuakWfT6LeCg4trqQ2YSy2q1pUCJppyPBFW89m3xZKhFgMhJgApkevYxYyn1GPDEpmuSUkYhDfEf68xrGNYAhEc';
-    private const TEST_RANDOM_NUMBER = 75;
-    // phpcs:enable
+beforeEach(function (): void {
+    $this->arionum = new Arionum(TestCase::TEST_NODE);
+});
 
-    /**
-     * This should never have enough funds.
-     *
-     * @test
-     * @return void
-     * @throws GenericApiException
-     */
-    public function itThrowsAnExceptionWhenSendingATransactionFromAnEmptyAccount(): void
-    {
-        $this->expectException(GenericApiException::class);
-        $this->expectExceptionMessage('Not enough funds');
+/** This should never have enough funds. */
+it('throws an exception when sending a transaction from an empty account', function (): void {
+    $testSendPublicKey = 'PZ8Tyr4Nx8MHsRAGMpZmZ6TWY63dXWSD1Hm7fGpQAgh1goGj8G47RmU68i3mP4erGGrJ1LNBzEy4di4jZKA2Z6ee96VxaDMUnzSthyzMSyhqF1DbLwNKPim2';
+    $testSendPrivateKey = 'Lzhp9LopCDbzk3eSdzuL5f9cR9ng12s6gNonQET3kSLtZU4MbQVreDRFjoWcEdUyeUN3tKwpR4AuakWfT6LeCg4trqQ2YSy2q1pUCJppyPBFW89m3xZKhFgMhJgApkevYxYyn1GPDEpmuSUkYhDfEf68xrGNYAhEc';
 
-        $transaction = new Transaction();
-        $transaction->changeValue(1.0);
-        $transaction->changeFee(1.0);
-        $transaction->changeDestinationAddress(self::TEST_ADDRESS);
-        $transaction->changePublicKey(self::TEST_SEND_KEY1);
-        $transaction->changeSignature('');
-        $transaction->changePrivateKey(self::TEST_SEND_KEY2);
-        $transaction->changeMessage('This should fail.');
-        $transaction->changeDate(time());
-        $transaction->changeVersion(Version::STANDARD);
+    $transaction = new Transaction();
+    $transaction->changeValue(1.0);
+    $transaction->changeFee(1.0);
+    $transaction->changeDestinationAddress(TestCase::TEST_ADDRESS);
+    $transaction->changePublicKey($testSendPublicKey);
+    $transaction->changeSignature('');
+    $transaction->changePrivateKey($testSendPrivateKey);
+    $transaction->changeMessage('This should fail.');
+    $transaction->changeDate(time());
+    $transaction->changeVersion(Version::STANDARD);
 
-        $this->arionum->sendTransaction($transaction);
-    }
+    $this->arionum->sendTransaction($transaction);
+})->throws(GenericApiException::class, 'Not enough funds');
 
-    /**
-     * @test
-     * @return void
-     * @throws GenericApiException
-     */
-    public function itGetsARandomlyGeneratedNumber(): void
-    {
-        $data = $this->arionum->getRandomNumber(1, 1, 100, self::TEST_NODE);
-        $this->assertEquals(self::TEST_RANDOM_NUMBER, $data);
-    }
+it('gets a randomly generated number', function (): void {
+    $testRandomNumber = 75;
 
-    /**
-     * @test
-     * @return void
-     * @throws GenericApiException
-     */
-    public function itGetsAListOfMasternodes(): void
-    {
-        $data = $this->arionum->getMasternodes();
-        $this->assertNotEmpty($data);
-    }
-}
+    $data = $this->arionum->getRandomNumber(1, 1, 100, TestCase::TEST_NODE);
+    expect($data)->toEqual($testRandomNumber);
+});
+
+it('gets a list of masternodes', function (): void {
+    $data = $this->arionum->getMasternodes();
+    expect($data)->not->toBeEmpty();
+});
